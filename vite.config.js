0d123cadfa09ba4dcd2cae3ce643d323ad051f95
ruleta-dev <flvckz@protonmail.com>
2026-07-27 23:18:37 -0300

    done

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,18 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: [
+      "@solana-program/token",
+      "@solana-program/memo",
+    ],
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        /^__vite-optional-peer-dep:@solana-program\/token.*$/,
+        /^__vite-optional-peer-dep:@solana-program\/memo.*$/,
+      ],
+    },
+  },
 });
