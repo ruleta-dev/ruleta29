65db0f75c3ddd0f36e3d8d8d0497b2181cf0fb5d
ruleta-dev <flvckz@protonmail.com>
2026-07-28 23:20:21 -0300

    done

---
+++ src/worker.js
@@ -0,0 +1,37 @@
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+
+    const ASSET_EXTENSIONS = [
+      ".js", ".css", ".json", ".png", ".jpg", ".jpeg", ".gif", ".svg", ".ico",
+      ".webp", ".woff", ".woff2", ".ttf", ".eot", ".map", ".html", ".txt",
+      ".xml", ".webmanifest", ".pdf", ".mp4", ".mp3", ".ogg", ".wasm"
+    ];
+
+    const hasTrailingSlash = url.pathname.endsWith("/");
+    const lastSegment = url.pathname.split("/").pop();
+    const hasExtension = lastSegment && lastSegment.includes(".") && 
+      ASSET_EXTENSIONS.some(ext => lastSegment.toLowerCase().endsWith(ext));
+
+    if (hasExtension) {
+      return env.ASSETS.fetch(request);
+    }
+
+    if (hasTrailingSlash) {
+      const indexUrl = new URL(url.pathname + "index.html", url.origin);
+      const indexReq = new Request(indexUrl, request);
+      const indexRes = await env.ASSETS.fetch(indexReq);
+      if (indexRes.status !== 404) {
+        return indexRes;
+      }
+    }
+
+    const rootIndexUrl = new URL("/index.html", url.origin);
+    const rootIndexReq = new Request(rootIndexUrl, request);
+    const rootRes = await env.ASSETS.fetch(rootIndexReq);
+    if (rootRes.status === 404) {
+      return env.ASSETS.fetch(request);
+    }
+    return rootRes;
+  },
+};
